c64b4ada4d8b42b915ee095d66562b5ed580aaa8
Harshdeep Mann <harshdeepsinghmann@gmail.com>
2017-02-01 20:47:48 -0800

    test update

--- gumapp.py
+++ gumapp.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "<h1 style='color:yellow'>Hello!! This is GumGum's first tech blog!!</h1>"
+    return "<h1 style='color:red'>Hi, I'm Harshdeep!! GumGum</h1>"
 
 if __name__ == "__main__":
     application.run(host='0.0.0.0')
