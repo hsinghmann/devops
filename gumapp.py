71ebcd625b3bd3be143e4a2094bfcb1168f56f01
Harshdeep Mann <harshdeepsinghmann@gmail.com>
2017-02-01 18:02:53 -0800

    new changes - color

--- gumapp.py
+++ gumapp.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "<h1 style='color:black'>Hello My name is Harshdeep! GumGum!!</h1>"
+    return "<h1 style='color:blue'>Hello!! This is GumGum's first tech blog!!</h1>"
 
 if __name__ == "__main__":
     application.run(host='0.0.0.0')
